620b9b44a578385464dc31c7e2f7cf04b8ba57d2
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2026-02-15 09:26:19 +0530

    hash the password using bcrypt

--- src/app.js
+++ src/app.js
@@ -2,18 +2,34 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 // create the new user
 app.post("/signup", async (req, res) => {
-  // create a new instance of the user model
-  const user = new User(req.body);
   try {
+    // 1. validation of data
+    validateSignUpData(req);
+    
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // 2. Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    // 3. create a new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.send("User added successfully!");
   } catch (err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("ERROR:" + err.message);
   }
 });
 
